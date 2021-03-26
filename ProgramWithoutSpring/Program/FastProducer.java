1239c257e1216a5d5b8947f6fd538a3b2bac6c2d
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2021-03-26 12:57:59 +0530

    Create FastProducer.java

---
+++ ProgramWithoutSpring/Program/FastProducer.java
@@ -0,0 +1,14 @@
+package Program;
+
+public class FastProducer implements Producer {
+
+	@Override
+	public void first() {
+		// TODO Auto-generated method stub
+		System.out.println("This is first Fast Producer Class method.");
+	}
+	public void second() {
+		System.out.println("This is second Fast Producer Class Method.");
+	}
+
+}
