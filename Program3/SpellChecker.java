e4ea4bcc4df6b41e64c2c54122493fa9b4442edb
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2021-03-26 09:39:05 +0530

    Create SpellChecker.java

---
+++ Program3/SpellChecker.java
@@ -0,0 +1,13 @@
+package Program3;
+
+public class SpellChecker {
+
+	public SpellChecker() {
+		// TODO Auto-generated constructor stub
+		System.out.println("SpellChecker");
+	}
+	public void display() {
+		System.out.println("SpellChecker Display");
+	}
+
+}
