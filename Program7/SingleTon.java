8d98007313ec000c2fa7e231d12a84ecdc498377
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2021-03-26 13:33:03 +0530

    Update SingleTon.java

--- Program7/SingleTon.java
+++ Program7/SingleTon.java
@@ -9,13 +9,12 @@ public class SingleTon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		ICouch coach=(ICouch)context.getBean("myTennisCoach");
-		coach.setName("Chirag");
-		ICouch coach1 = (ICouch)context.getBean("myTennisCoach");
-		coach1.setName("Sardana");
-//		System.out.println("The Name of the Coach is : ");
+		ICoach coach=(ICoach)context.getBean("myTennisCoach");
+		coach.setName("Prakash");
+		ICoach coach1 = (ICoach)context.getBean("myTennisCoach");
+		coach1.setName("Singh");
 		System.out.println(coach+"\n"+coach1);
-		System.out.println("The Object is equal : "+coach.equals(coach1));//CHecking whether it refers to same memory location or not
+		System.out.println("The Object is equal : "+coach.equals(coach1));
 		
 		System.out.println(coach.getName()+"\n"+coach1.getName());
 	}
