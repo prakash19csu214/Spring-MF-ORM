efc56d859817d52079812b334a5ba3ad4aae50ac
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2021-03-26 09:59:25 +0530

    Create ICoach.java

---
+++ Program7/ICoach.java
@@ -0,0 +1,8 @@
+package Program7;
+
+public interface ICoach {
+	public String getDailyWorkout();
+	public String getDailyFortune();
+	public String getName();
+	public void setName(String name);
+}
