1da21ed4dacad1b36d7bd99c7e65d8292454d806
Prakash Singh <70188303+prakash19csu214@users.noreply.github.com>
2021-03-26 12:09:28 +0530

    Update MainClass.java

--- Program7/MainClass.java
+++ Program7/MainClass.java
@@ -3,14 +3,14 @@ package Program7;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import Program2.ICouch;
+import Program2.ICoach;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-		ICouch coach=(ICouch)context.getBean("myCricketCoach");
+		ICoach coach=(ICoach)context.getBean("myCricketCoach");
 		
 		//Using Constructor Method
 		System.out.println("Using Contructor with Address Excluded.");
@@ -21,13 +21,13 @@ public class MainClass {
 		
 		//Using Setter Method
 		System.out.println("Using Setter with Address Included.");
-		ICouch coach1=(ICouch)context.getBean("myCricketCoach1");
+		ICoach coach1=(ICoach)context.getBean("myCricketCoach1");
 		System.out.println(coach1.getDailyWorkout());
 		System.out.println(coach1.getDailyFortune());
 		System.out.println(coach1+"\n\n");
 
 		System.out.println("Using Contructor with Address Included.");
-		ICouch coach2 = (ICouch)context.getBean("myCricketCoach2");
+		ICoach coach2 = (ICoach)context.getBean("myCricketCoach2");
 		System.out.println(coach2.getDailyWorkout());
 		System.out.println(coach2.getDailyFortune());
 		System.out.println(coach2+"\n\n");
